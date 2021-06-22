23a457d4ef7d00c9a7fd304a541c1a26a1be05bc
adebayo935 <85680990+adebayo935@users.noreply.github.com>
2021-06-22 14:30:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 from all_books import scrape_all_books
 from category import scrape_category
+import os
 
+if not os.path.exists("Category"):
+    os.mkdir("Category")
+if not os.path.exists("All Books"):
+    os.mkdir("All books")
 
 print("Main menu\n")
 print("Tape 1 to scrape all books of the site\n")
