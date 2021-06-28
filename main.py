a1b346da15a7cbbe5761c6f1feb691466137950c
adebayo935 <85680990+adebayo935@users.noreply.github.com>
2021-06-28 19:16:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,19 @@
 from all_books import scrape_all_books
 from category import scrape_category
 import os
+import shutil
 
 if not os.path.exists("Category"):
     os.mkdir("Category")
+else:
+    shutil.rmtree("Category")
+    os.mkdir("Category")
+
 if not os.path.exists("All Books"):
     os.mkdir("All books")
+else:
+    shutil.rmtree("All Books")
+    os.mkdir("All Books")
 
 print("Main menu\n")
 print("Tape 1 to scrape all books of the site\n")
